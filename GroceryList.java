171631fbaf168f8f3e28f6a0e8937ffa26a0e8c3
Hawa Drammeh <hawad416@gmail.com>
2021-01-14 17:26:51 -0800

    Some grocery files

---
+++ GroceryList.java
@@ -0,0 +1,26 @@
+// This class keeps track of a grocery list of items
+
+public class GroceryList {
+    public static final int MAX_ITEMS = 10;
+
+    private GroceryItemOrder[] items;
+    private int numItems;
+
+    public GroceryList() {
+        items = new GroceryItemOrder[MAX_ITEMS];
+        numItems = 0;
+    }
+
+    public void add(GroceryItemOrder item) {
+        items[numItems] = item;
+        numItems++;
+    }
+
+    public double getTotalCost() {
+        double cost = 0.0;
+        for (int i = 0; i < numItems; i++) {
+            cost += items[i].getCost();
+        }
+        return cost;
+    }
+}
---
+++ Grocery_main.java
@@ -0,0 +1,32 @@
+package GroceryList;
+
+public class Grocery_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		GroceryList myList = new GroceryList();
+		
+		
+		GroceryItemOrder item1 = new GroceryItemOrder("rice 1", 5, 9.95 );
+		GroceryItemOrder item2 = new GroceryItemOrder("salad", 1, 3.00 );
+		GroceryItemOrder item3 = new GroceryItemOrder("chips",3, 3.5 );
+		GroceryItemOrder item4 = new GroceryItemOrder("cookies",1, 2.00 );
+		GroceryItemOrder item5 = new GroceryItemOrder("flowers",1, 9.95 );
+		GroceryItemOrder item6 = new GroceryItemOrder("milk",1, 9.95 );
+		GroceryItemOrder item7 = new GroceryItemOrder(" gum",1, 9.95 );
+		
+		
+		myList.add(item1);
+		myList.add(item2);
+		myList.add(item3);
+		myList.add(item4);
+		myList.add(item5);
+		myList.add(item6);
+		myList.add(item7);
+		
+		myList.displayItems();
+		//System.out.println("Total = " + myList.getTotalCost());
+	}
+
+}
